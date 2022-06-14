da1cc59f8ff9e60541ac55a3b3a9ffb09cd6a889
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-06-14 00:09:04 +0000

    Created V1 and V2 of the Recipe not blank component

---
+++ 01_recipe_not_blank/recipe_not_blank_v1.py
@@ -0,0 +1,25 @@
+#14/6/22 - KLB
+#Recipe Cost Calculator
+#Recipe not blank V1
+#Aim - To get a basic version of recipe not blank 
+
+
+#Functions   
+def not_blank(question):
+  valid = False
+
+  while not valid:
+    response = input(question)
+
+    if response != "":
+      return response
+
+    else:
+      print("Sorry - this can't be blank, "
+            "please enter your recipe name")
+
+#Main Routine
+
+name= not_blank("Recipe: ")
+  
+     
---
+++ 01_recipe_not_blank/recipe_not_blank_v2.py
@@ -0,0 +1,25 @@
+#14/6/22 - KLB
+#Recipe Cost Calculator
+#Recipe not blank V2
+#Aim - To get a basic version of recipe not blank but allow a more customisable error message that can be used for different questions
+
+
+
+
+#Functions   
+def not_blank(question, error_message):
+  valid = False
+
+  while not valid:
+    response = input(question)
+
+    if response != "":
+      return response
+
+    else:
+      print(error_message)
+
+#Main Routine
+
+name= not_blank("Recipe: ", "Sorry- this can't be blank, please enter your recipe name")
+  
