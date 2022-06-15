a7a56221fa147eca0f893d3a4a0ce9aebf329465
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-06-15 02:15:45 +0000

    Created V3 and V4 of the float checker code. However I plan to  change V4 so it is more flexible

---
+++ 02_float_checker/float_checker_v3.py
@@ -0,0 +1,34 @@
+#15/6/22 - KLB
+#Recipe Cost Calculator
+#Float Checker V3
+#Aim - To create a function that has more specific error messages
+
+
+def float_checker(question): 
+  valid = False
+  while not valid:
+
+   #ask user for number and checks if it is valid
+   try:
+    response = float(input(question))
+  #Checks if number is negative
+    if response > 0:
+      valid = True
+  
+    else:
+     print("Negative Number! Please enter a valid serving size")
+      
+   #Errror prevention for if a string is entered
+   except:
+     print("Invalid number! Please enter the serving size as a number")
+
+  return response
+      
+
+
+
+
+
+# Main Routine 
+serving_size = float_checker("Serving size: ")
+print(serving_size)
---
+++ 02_float_checker/float_checker_v4.py
@@ -0,0 +1,32 @@
+#15/6/22 - KLB
+#Recipe Cost Calculator
+#Float Checker V4
+#Aim - To create a function that still has specific error messages but would be more flexible when the function is used for different questions
+
+
+def float_checker(question, string_error_message, neg_num_error_message): 
+  valid = False
+  while not valid:
+
+   #ask user for number and checks if it is valid
+   try:
+    response = float(input(question))
+
+    if response > 0:
+      valid = True
+   #Checks if number is in range
+    else:
+     print(neg_num_error_message)
+   except:
+     print(string_error_message)
+
+  return response
+      
+
+
+
+
+
+# Main Routine 
+serving_size = float_checker("Serving size: ", "Invalid number! Please enter the serving size as a number", "Negative Number! Please enter a valid serving size")
+print(serving_size)
