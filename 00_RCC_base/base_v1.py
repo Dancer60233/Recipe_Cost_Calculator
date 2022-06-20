6b189e1631960aa4f85cea17baa8f4d3935be9a8
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-06-20 03:11:14 +0000

    Created base v1 code

--- 00_RCC_base/base_v1.py
+++ 00_RCC_base/base_v1.py
@@ -1,4 +1,71 @@
-#10/6/22 - KLB
+#20/6/22 - KLB
 #Recipe Cost Calculator
 #Base V1
-#Aim -
+#Aim - To create a version of my code that has all functions and my basic decomposition. So even though it won't work currently it'll make it easier to add components
+
+
+#------------Functions---------------
+
+def not_blank(question):
+ print(question)
+
+def float_checker(question):
+  print(question)
+
+def string_analyser(question):
+  print(question)
+
+def calc_ing_cost():
+  print("calculate ingredient cost")
+
+def calc_serving_cost():
+  print("Cost Per Serving")
+
+def space(num_spaces):
+ for item in range (0, num_spaces):
+   print()
+
+  
+
+
+
+
+
+#*************** Main Routine *************
+
+#Temporary number of ingredients will ask user in future versions
+num_ing = 3
+
+if __name__ == "__main__":
+
+ #Welcome users
+ print("Welcome to the Recipe Cost Calculator")
+
+#Ask user for basic details about recipe
+ recipe_name = not_blank("Recipe Name:")
+ serving_size = float_checker("Sering Size:")
+
+ space(1)
+
+#Loop for number of ingredients
+ for item in range(0, num_ing):
+   
+   #Ask for information about ingredient
+   ing_name = not_blank("Ingredient Name: ")
+   amount_ing_need = string_analyser("Amount Ingredient Needed:")
+   total_amount_ing = string_analyser("Total Amount of Ingredient Purchased:")
+   ing_price = float_checker("Ingredient Price")
+   
+   #Calculate cost of ingredient needed
+   calc_ing_cost()
+
+   space(1)
+
+#Calculate cost per serving
+ cost_per_serving = calc_serving_cost()
+
+#Output results
+ print("Total cost is {}".format(cost_per_serving))
+
+   
+ 
