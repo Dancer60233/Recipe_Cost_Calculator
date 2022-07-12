14a18220ba348df36d1e6dfbd9c2036685ea88fc
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-07-12 10:24:36 +0000

    Worked on Base V4

--- 00_RCC_base/base_v4.py
+++ 00_RCC_base/base_v4.py
@@ -1,7 +1,7 @@
-#20/6/22 - KLB
+#12/07/22 - KLB
 #Recipe Cost Calculator
 #Base V4
-#Aim - To create a new way of asking for amount of ingredient by using str
+#Aim - To add my amount analyser component to the base and get it to work
 
 #------------Functions---------------
 
@@ -39,14 +39,114 @@ def float_checker(question, type):
   return response
       
 
+def string_check(choice, options):
+ for var_list in options:
+   #if snack is in one of the lists, return the full name
+      if choice in var_list:
+  #Get full name of snack and put it in titles case so it looks nice when outputted
+       return var_list[0].lower()
+       is_valid = "yes"
+       break
+
+      else:
+        is_valid = "no"
+
+ if is_valid == "no":
+   return "invalid choice"
+   
+   
+ else:
+    return "invalid choice"
+  
 
 
 
 
-# Main Routine 
 
-def string_analyser(input):
- print("Place holder")
+
+
+
+
+
+def amount_analyser(ingredient):
+
+  unit = "invalid choice"
+  amount = "invalid choice"
+  
+  
+  while unit == "invalid choice" or amount == "invalid choice":
+   desired_amount = ""
+   desired_unit = ""
+
+  #Ask user for ingredient amount
+   ingredient_amount = input("Ingredient Amount: ")
+
+  #Seperate amount and unit
+   for m in ingredient_amount:
+     
+    if m.isdigit() or m == "." or m == "-":
+        desired_amount = desired_amount + m
+        
+    else:
+      desired_unit = desired_unit + m
+
+   desired_amount = desired_amount.strip()
+   desired_unit = desired_unit.strip().lower()
+
+  #Use string check to see if unit is valid
+   unit = string_check(desired_unit, valid_units)
+
+
+  #Check if Amount is valid
+   if desired_amount == "":
+     amount = "invalid choice"
+
+   else:
+    amount = float(desired_amount)
+    if amount < 0:
+     amount = "invalid choice"
+     
+  #Print out of error message if needed
+
+      #Error message if unit and amount invalid
+   if unit == "invalid choice" and amount == "invalid choice":
+      print("Invalid Choice! Please enter a valid unit and amount")
+     
+   
+       #Error message if unit invalid
+   elif unit == "invalid choice":
+      print("Invalid Choice! Please enter a valid unit")
+    #Error message if  amount invalid
+   elif amount == "invalid choice":
+      print("Invalid Choice! Please enter a valid amount")
+
+  #Convert amount into grams
+   if unit == "g":
+    gram_amount = amount
+   elif unit == "kg":
+    gram_amount = amount * 1000
+   elif unit == "tsp":
+    gram_amount = amount * 5.69
+   elif unit == "tbsp":
+    gram_amount = amount * 17.07 
+   elif unit == "eggs":
+    gram_amount = "egg"
+   elif unit == "cups":
+    if ingredient == "butter":
+      gram_amount = amount * 250
+    elif ingredient == "sugar":
+      gram_amount = amount * 250
+    elif ingredient == "flour":
+      gram_amount = amount * 125
+    elif ingredient == "oil":
+     gram_amount = amount * 220
+    elif ingredient == "water":
+     gram_amount = amount * 236 
+    else:
+      gram_amount = amount * 250
+     
+  return amount, unit, gram_amount
+ 
 
 def calc_ing_cost():
   print("calculate ingredient cost")
@@ -61,10 +161,25 @@ def space(num_spaces):
   
 
 
+
+
+
 #*************** Main Routine *************
 
 #Temporary number of ingredients will ask user in future versions
 num_ing = 3
+all_amounts = []
+all_units = []
+
+
+valid_units =[
+  ["g", "grams", "gram" ],
+  ["kg", "kilograms", "kilogram", "kilos", "kilo"],
+  ["tbsp", "tablespoon", "tablespoons"],
+  ["tsp", "teaspoon", "teaspoons"],
+  ["cups", "cup"],
+  ["eggs", "egg"]
+]
 
 if __name__ == "__main__":
 
@@ -86,12 +201,14 @@ if __name__ == "__main__":
 
    
    #Amount of ingredient needed
-   amount_ing_need = input("Amount Ingredient Needed: ")
-   amount_ing_need = string_analyser(amount_ing_need)
+   need_amount, need_unit, need_amount_in_g = amount_analyser(ing_name)
+   all_amounts.append(need_amount)
+   all_units.append(need_unit)
+
 
    #Total Amount of ingredient purchased
-   total_amount_ing = input("Total Amount of Ingredient Purchased: ")
-   total_amount_ing = string_analyser(total_amount_ing)
+   total_amount, total_unit, total_amount_in_g = amount_analyser(ing_name)
+  
 
    #Ingredient Price
    ing_price = float_checker("Ingredient Price: $", "price")
