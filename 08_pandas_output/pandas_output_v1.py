050bf54e2b296e023617ca3a11d3e1b0ecc7d75b
Kate Barber <101075108+Dancer60233@users.noreply.github.com>
2022-07-17 10:42:36 +0000

    Completed Base V7 and got it to function

---
+++ 08_pandas_output/pandas_output_v1.py
@@ -0,0 +1,21 @@
+#17/7/22 - KLB
+#Recipe Cost Calculator
+#Pandas Output V1
+#Aim - To create a basic table that doesn't include the title or total to set up the basic dictionary
+
+import pandas
+
+all_ing_names = []
+all_prices = []
+pita_chips = []
+water = []
+orange_juice = []
+
+
+recipe_cost_dict = {
+    'Ingredient Name': all_ing_names,
+    'Price': popcorn,
+    'Amount Needed': water,
+    'Amount Total': pita_chips,
+    'Ingredient Cost': mms,
+    }
